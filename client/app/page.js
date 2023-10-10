a000c128df55daa08a941f35fc72d7da26827cf6
khushi gupta <guptakhushi622003@gmail.com>
2023-10-10 18:39:31 +0530

    Created the client side Design

---
+++ client/app/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Mainpage from '@/component/Mainpage/Mainpage'
+
+const page = () => {
+  return (
+    <div>
+      <Mainpage/>
+    </div>
+  )
+}
+
+export default page
